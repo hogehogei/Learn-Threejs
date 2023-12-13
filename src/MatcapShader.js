6617e6cd01b07d13fb774b2d7f352e3394131e80
hogehogei <matsuryo00@gmail.com>
2023-12-13 23:09:16 +0900

    MatCapシェーダ実装

---
+++ src/MatcapShader.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three'
+
+export const MatcapShader = {
+  glslVersion: THREE.GLSL3,
+  uniforms: {
+    matcap: { value: null },
+  },
+  vertexShader: /* glsl */ `
+    out vec3 v_normal;
+
+    void main() {
+      v_normal = normalMatrix * normal;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  fragmentShader: /* glsl */ `
+    in vec3 v_normal;
+
+    uniform sampler2D matcap;
+    
+    out vec4 fragColor;
+
+    void main() {
+      vec2 uv = v_normal.xy * 0.5 + 0.5;
+      fragColor = texture(matcap, uv);
+    }
+  `,
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { FBXLoader } from 'three/addons/loaders/FBXLoader.js'
+import { MatcapShader } from './MatcapShader.js'
+
+function createMeshPhongMaterial()
+{
+    const material = new THREE.MeshPhongMaterial();
+    material.shininess = 100;
+    material.specular = new THREE.Color(0x1188ff);
+    return material;
+}
+
+function createMatcapMaterial(image)
+{
+    console.log( 'load image: %s', image );
+    const matcapTexture = new THREE.TextureLoader().load(image);
+    const material = new THREE.ShaderMaterial(MatcapShader);
+    material.uniforms.matcap.value = matcapTexture;
+
+    return material;
+}
 
 // サイズを指定
-const width = 960;
-const height = 540;
+const width = 1482;
+const height = 920;
 
 // レンダラーを作成
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector("#myCanvas"),
+    //alpha: true,
+    antialias: true
 });
 renderer.setSize(width, height);
 
@@ -14,31 +37,59 @@ renderer.setSize(width, height);
 const scene = new THREE.Scene();
 
 // カメラを作成
-const camera = new THREE.PerspectiveCamera(45, width / height);
-camera.position.set(0, 0, +1000);
+// new THREE.PerspectiveCamera(視野角, アスペクト比, near, far)
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.0001, 1000);
+// 焦点距離設定
+camera.setFocalLength(50);
+camera.position.set(0, 0, +5);
+// カメラコントローラーを作成
+const controls = new OrbitControls(camera, document.body);
+// 滑らかにカメラコントローラーを制御する
+controls.enableDamping = true;
+controls.dampingFactor = 0.2;
 
-// 球体を作成
-const geometry = new THREE.SphereGeometry(300, 30, 30);
-// マテリアルを作成
-const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-// メッシュを作成
-const mesh = new THREE.Mesh(geometry, material);
-// 3D空間にメッシュを追加
-scene.add(mesh);
+
+const material = createMatcapMaterial("../texture/matcap3.png");
+
+const fbxLoader = new FBXLoader()
+fbxLoader.load(
+    '../model/Luna.fbx',
+    (object) => {
+        object.traverse( (child) => {
+            if( child.isMesh ){
+                child.material = material;
+                if( child.material ){
+                    child.material.transparent = false;
+                }
+            }
+        });
+        object.scale.set(1, 1, 1);
+        scene.add(object);
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+    },
+    (error) => {
+        console.log(error);
+    }
+)
 
 // 平行光源
-const directionalLight = new THREE.DirectionalLight(0xffffff);
-directionalLight.position.set(1, 1, 1);
+//const directionalLight = new THREE.DirectionalLight(0xffffff);
+//directionalLight.position.set(1, 1, 1);
 // 環境光源
-const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.3)
+//const ambientLight = new THREE.AmbientLight(null, 0.3);
 // シーンに追加
-scene.add(directionalLight);
-scene.add(ambientLight);
+//scene.add(directionalLight);
+//scene.add(ambientLight);
 
 tick();
 
-// 毎フレーム時に実行されるループイベントです
+// 毎フレーム時に実行されるループイベント
 function tick() {
+    // カメラコントローラーを更新
+    controls.update();
+
     // レンダリング
     renderer.render(scene, camera);
 
